d529b64ef29f18af1a9df4413d907b8a5a4fcef6
Mayeldito <gomez.delgado.miguel.a@gmail.com>
2022-10-23 15:34:17 -0500

    Se agregaron los booleanos para el juego asi como el mensaje de inicio y el primer input

--- pipati.py
+++ pipati.py
@@ -1,1 +1,17 @@
-print("prueba de ejecucion de py en la terminal, si esto aparece todo bien")
+#print("prueba de ejecucion de py en la terminal, si esto aparece todo bien")
+
+Rock and Scissors
+
+Paper and scissors
+
+Scissors and scissors
+
+print(Bienvenido al juego de piedra, peapel o tijera, el gran juego aclamado a nivel muncdial)
+
+input("Por favor elige tu objeto: 
+1. Scissors
+2. Rock
+3. Paper")
+
+attack = input
+response = scissors
